b33e658cea423da8676ed91a9144fa5651a1255f
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-11-25 00:29:16 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,44 @@
-const express = require("express")
+const express = require("express");
 const { Server } = require("socket.io");
-var http = require('http');
-const cors = require("cors")
+var http = require("http");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // Change this to the appropriate origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 var server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get("/", (req, res) =>
- {
+app.get("/", (req, res) => {
   res.send("Chat BE with Socket.io");
-  res.end()
-})
+  res.end();
+});
 
 io.on("connection", (socket) => {
-  console.log(socket.id)
+  console.log(socket.id);
 
-  socket.on("joinRoom", room => {
-		socket.join(room)
-  })
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+  });
 
-  socket.on("newMessage", ({newMessage, room}) => {
-    io.in(room).emit("getLatestMessage", newMessage)
-  })
-
+  socket.on("newMessage", ({ newMessage, room }) => {
+    io.in(room).emit("getLatestMessage", newMessage);
+  });
 });
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
 
-server.listen(port, console.log(`web started at port ${port}`))
+server.listen(port, console.log(`web started at port ${port}`));
